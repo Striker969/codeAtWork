6d0d9d876c319fe9b84b667602c4c244cbf7e23a
Balu <roopak.balu@pditechnologies.com>
2026-04-03 09:15:32 +0530

    unique paths and more

--- leetCodePrograms.java
+++ leetCodePrograms.java
@@ -99,6 +99,24 @@ class Solution {
         return maxnum;
     }
 }
+/**OR */
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            if (sum > 0) {
+
+                max = Math.max(sum, max);
+            }
+        }
+        return max;
+    }
+}
 
 
 
@@ -183,8 +201,10 @@ class Solution {
 
 
 
-
+//To find the duplicate number
 /**
+ * //this is Array → Linked List Conversion Trick
+ * rule is this index → value → next index
  * What is a Pointer in a Linked List?
  * 1. Data (value)
  * 2. Pointer (reference to the next node)
@@ -203,6 +223,10 @@ class Solution {
  * TC - O(n) and SC - O(1)
  */
 //using Floyd Cycle Detection Algorithm we can solve this problem
+//We treat the array as a linked list where each value points to the next index. Because there is a duplicate number, two indices will point to the same value, creating a cycle. When slow and fast pointers meet, it confirms the presence of that cycle.
+
+//input [1,3,4,2,2]
+//output = 2
 class Solution {
     public int findDuplicate(int[] nums) {
         int a=nums[0];
@@ -210,7 +234,10 @@ class Solution {
         //phase 1
         //The slow and fast pointers will meet somewhere inside the cycle ,
         while(a!=b){
+            //slow → moves 1 step
             a=nums[a];
+            //fast → moves 2 steps
+            //fast will eventually catch slow
             b=nums[nums[b]];
         }
         a=0;
@@ -274,6 +301,177 @@ class Solution {
             x.put(sum,x.getOrDefault(sum,0)+1);
         }
         return count;
+    }
+}
+
+
+//To count the frequency of the elements(Integer)
+//input = [1,2,2,3,1,2]
+//output = {1=2, 2=3, 3=1}
+//O(n)
+class Main {
+    public static void main(String[] args) {
+        int a[]={1,2,2,3,1,2};
+        HashMap<Integer, Integer> x=new HashMap<>();
+        for(int i=0;i<a.length;i++){
+            x.put(a[i],x.getOrDefault(a[i],0)+1);
+        }
+        System.out.print(x);
+    }
+}
+
+
+//To count the frequency of the elements(String)
+//input - aabbc
+//input = {a=2, b=2, c=1}
+//O(n)
+class Main {
+    public static void main(String[] args) {
+        String a="aabbc";
+        HashMap<Character, Integer> x=new HashMap<>();
+        for(char tt:a.toCharArray()){
+            x.put(tt,x.getOrDefault(tt,0)+1);
+        }
+        System.out.print(x);
+    }
+}
+
+
+//more optimized(String)
+//TC=O(n) and SC-O(1)
+class Main {
+    public static void main(String[] args) {
+        String a="aabbc";
+        int b[]=new int[26];
+        for(int i=0;i<a.length();i++){
+            b[a.charAt(i)-'a']++;
+        }
+        for(int i=0;i<b.length;i++){
+            if(b[i]>0){
+                char ch=(char)(i+'a');
+                System.out.print(ch+" "+b[i]+" ");
+            }
+        }
+    }
+}
+
+
+//to find all the duplicates in the array
+//TC=O(n) and SC=O(1)
+
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> x=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            //Math.abs is used because the digits will become negative
+            int val=Math.abs(nums[i]);
+            //converts value into valid index because if the higest value in the array is 8 then i need a index of 7 so val-1 is done
+            int index=val-1;
+            if(nums[index]<0){
+                x.add(val);
+            }else{
+                nums[index]=-nums[index];
+            }
+        }
+        return x;
+    }
+}
+
+
+//Rotate array(right rotate)
+//TC=O(n) and SC=O(1)
+//input = [1,2,3,4,5,6,7]
+//output = [5,6,7,1,2,3,4]
+//k=3
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        int left=n-k;
+        int right=n-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+        left=0;
+        right=n-k-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+        left=0;
+        right=n-1;
+        while(left<right){
+            int temp=nums[left];
+            nums[left]=nums[right];
+            nums[right]=temp;
+            left++;
+            right--;
+        }
+        System.out.print(nums.toString());
+    }
+}
+
+
+
+//merge the sorted array
+//TC-O(m+n) and SC=O(n)
+//TC is O(m+n) because Each element is processed once, 👉 Each iteration: Either i-- OR j--
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=m-1;
+        int j=n-1;
+        int k=m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i]>=nums2[j]){
+                nums1[k--]=nums1[i--];
+            }else{
+                nums1[k--]=nums2[j--];
+            }
+        }
+        while(j>=0){
+            nums1[k--]=nums2[j--];
+        }
+    }
+}
+
+
+
+//To find the missing number
+//TC=O(n) and SC=O(1)
+/**
+ * Expected: 0 ^ 1 ^ 2 ^ 3
+Actual:   3 ^ 0 ^ 1
+
+Combine:
+0 ^ 1 ^ 2 ^ 3 ^ 3 ^ 0 ^ 1
+
+Cancel:
+(0^0)(1^1)(3^3) → 2 remains
+
+input - [3,0,1]
+output - 2
+ */
+class Solution {
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int xor = 0;
+
+        // XOR all indices and values
+        for (int i = 0; i < n; i++) {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        // XOR with n
+        //this is important because the length is 3 and it is not included in XORing
+        return xor ^ n;
     }
 }
 
@@ -497,6 +695,8 @@ class Solution {
 
 //Longest substring without repeating characters
 //TC-O(n) abd SC-O(min(n, charset))
+//input - abcabcbb
+//output - 3
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         HashMap<Character, Integer> map =new HashMap<>();
@@ -505,7 +705,7 @@ class Solution {
         for(int right=0;right<s.length();right++){
             char t=s.charAt(right);
             if(map.containsKey(t)){
-                //map.get(t)+1 is used to get the length of the chracter if i dont use +1 then i will just ge the index
+                //map.get(t)+1 is used to get the length of the chracter if i dont use +1 then i will just get the index
                 left=Math.max(left,map.get(t)+1);
             }
             map.put(t,right);
@@ -517,11 +717,15 @@ class Solution {
 }
 
 
-/***********************************************Binary search programs************************************************/
+/*********************************************Binary search programs************************************************/
 
 
 //Binary search
 //TC - O(log n) and SC - O(1)
+//input - [1,2,3,4,5,6,7,8,9,10] and target is 5
+//output - 4 (at fourth place)
+//input - [1,2,3,4,5,6,7,8,9,10] and target is 11
+//output - -1 (not found)
 class Solution {
     public int search(int[] nums, int target) {
         int low =0;
@@ -540,6 +744,125 @@ class Solution {
         return -1;
     }
 }
+
+//search in a rotated sorted array
+/**
+ * At every step:
+- We eliminate HALF of the array
+- 👉 That’s why:
+- Time Complexity = O(log n)
+ space complexity = O(1)
+ "At each step, I determine which half is sorted. Since one half is always sorted in a rotated array, I check if the target lies within that sorted range. If it does, I discard the other half; otherwise, I discard the sorted half. This way, I eliminate half of the search space in every iteration."
+ */
+//TC - O(log n) and SC - O(1)
+//input - [4,5,6,7,0,1,2] and target is 0
+//output - 4 (at fourth place)
+//input - [4,5,6,7,0,1,2] and target is 3
+//output - -1 (not found)
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if (nums[mid]==target) return mid;
+            //check if the left half is sorted
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target&&target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
+                //if the target is not in the left half, then it must be in the right half
+                // else condition will be -> else if(nums[mid]<=nums[high])
+            }else{
+                if(nums[mid]<=target&&target<=nums[high]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
+
+
+//search insert position
+//TC - O(log n) and SC - O(1)
+//input - [1,3,5,6] and target is 5
+//output - 2 (at second place)
+//input - [1,3,5,6] and target is 2
+//output - 1 (at first place)
+//When the loop ends, low points to the first index where the target can be inserted without breaking the sorted order, because all elements before low are smaller and all elements after are greater.
+//there is a point where the while loop breaks, if low>high then low will be the index where the target can be inserted. 
+//because all elements before low are smaller and all after are greate so we return low.
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low =0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target) {
+                return mid;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return low;
+    }
+}
+
+
+//To find the peak element in the array
+//"I compare nums[mid] with nums[mid+1] to determine the slope. If it's increasing, I move right; otherwise, I move left. This guarantees finding a peak in O(log n)."
+//We don’t search peak directly, We follow slope → peak automatically reached
+//because all elements before low are smaller and all after are greater so we return low.
+//TC - O(log n) and SC - O(1)
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low=0;
+        int high =nums.length-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return low;
+    }
+}
+
+
+
+//find minimum in rotated sorted array
+//TC - O(log n) and SC - O(1)
+class Solution {
+    public int findMin(int[] nums) {
+        int ans=Integer.MAX_VALUE;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            //check if the left half is sorted
+            if(nums[low]<=nums[mid]){
+                //if the left half is sorted, then the minimum element is in the left half and ignore the left half
+                ans=Math.min(ans,nums[low]);
+                low=mid+1;
+            }else{
+                //check if the right half is sorted
+                ans=Math.min(ans,nums[mid]);
+                high=mid-1;
+            }
+        }
+        return ans;
+    }
+}
+
 
 //**************************************************Two pointers******************************************************
 //Two Sum 2(using two pointer technique as the array is sorted, to optimize the space that is why we are not using the hasmap here), 
@@ -586,6 +909,7 @@ class Solution {
 //Move zeros to the end of the array
 //Input: nums = [0,1,0,3,12]
 //Output: [1,3,12,0,0]
+//I can use either while or for loop here. Since I'm filling a known range, a for loop is cleaner and more readable.
 class Solution {
     public void moveZeroes(int[] nums) {
         int ind=0;
@@ -624,5 +948,162 @@ class Solution {
             }
         }
         return max;
+    }
+}
+
+
+/*******************************************************OTHERS********************************************************/
+
+
+
+//FizzBuzz
+/**
+ * 
+ * answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+ *  answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+
+ * Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ */
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> x=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3==0 && i%5==0){
+                x.add("FizzBuzz");
+            }else if(i%3==0){
+                x.add("Fizz");
+            }else if(i%5==0){
+                x.add("Buzz");
+            }else{
+                x.add(String.valueOf(i));
+            }
+        }
+        return x;
+    }
+}
+
+
+/************************************************Dynamic programming*************************************************/
+//Unque path
+//TC and SC=O(mxn)
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int grid[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0)
+                grid[i][j]=1;
+                else
+                grid[i][j]=grid[i][j-1]+grid[i-1][j];
+            }
+        }
+        //due to 0 based indexing
+        return grid[m-1][n-1];
+    }
+}
+
+
+
+//Climbing stairs(fibonacci [pattern])
+//TC=O(n) and SC=O(1)
+//n=8
+//optimized
+class Solution {
+    public int climbStairs(int n) {
+
+        if (n <= 2) return n;
+
+        int prev1 = 2, prev2 = 1;
+
+        for (int i = 3; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
+//or
+//TC=O(n) and SC=O(n)
+class Solution {
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
+
+
+
+//House robber
+//TC=O(n) and SC=O(n)
+class Solution {
+    public int rob(int[] nums) {
+        int r[]=new int[nums.length];
+        if (nums.length<2) 
+        return nums[0];
+
+        r[0]=nums[0];
+        r[1]=Math.max(nums[0], nums[1]);
+        for(int i=2;i<nums.length;i++){
+            r[i]=Math.max(r[i-2]+nums[i],r[i-1]);
+        }
+        return r[nums.length-1];
+    }
+}
+//or
+//optimized SC=O(1)
+class Solution {
+    public int rob(int[] nums) {
+
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            int curr = Math.max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
+
+
+
+//Minimum cost climbing stairs
+//TC and SC=O(n)
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int mincost[]=new int[n+1];
+
+        for(int i=2;i<=n;i++){
+            mincost[i]=Math.min((cost[i-1]+mincost[i-1]),(cost[i-2]+mincost[i-2]));
+        }
+
+        return mincost[n];
     }
 }
